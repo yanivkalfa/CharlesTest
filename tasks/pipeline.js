6dbc8d0b1980ca553084052a09f79156cde42feb
Yaniv-Kalfa <netameta1@yahoo.com>
2014-09-29 23:12:43 -0400

    model setup

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -31,7 +31,7 @@ var jsFilesToInject = [
     '/bower/jquery/dist/jquery.min.js',
     '/bower/jquery-ui/jquery-ui.min.js',
     '/bower/angular/angular.min.js',
-    '/bower/angular-sanitize/angular-sanitize.min.js',
+    //'/bower/angular-sanitize/angular-sanitize.min.js',
 
     '/js/_customers.js',
     '/js/app.js',
