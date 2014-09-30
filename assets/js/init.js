380a04aed54aad98dd8ab8d205b1fb416efcd850
Yaniv-Kalfa <netameta1@yahoo.com>
2014-09-29 23:06:16 -0400

    model setup

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,15 +1,62 @@
-jQuery(function(){
+(function (undefined) {
+    'use strict';
 
-    $('textarea').autoresize();
 
-    $('.datepicker').datepicker({
-        showWeek: false,
-        dayNames: [],
-        dateFormat: "dd/mm/yy",
-        onSelect : function(date,object){
-            var altField = $(this).siblings('.altField');
-            var dateObject = $( this ).datepicker( "getDate" );
-            altField.val(moment(dateObject).format("MM/DD/YYYY"));
-        }
-    });
-});
+    function api() {
+        var _data = {
+            users: [
+                {
+                    id: 142,
+                    name: 'Jack',
+                    type: 'admin',
+                    age: 32,
+                    skills: ['unix', 'windows', 'http']
+                },
+                {
+                    id: 523,
+                    name: 'Bob',
+                    type: 'customer',
+                    age: 23,
+                    likes: ['shoes', 't-shirts', '<b>Clubs</b>&nbsp;<br/><i>bars</i>']
+                }
+            ],
+
+            responsibilities: {
+                142: [
+                    {
+                        name: 'project 1'
+                    },
+                    {
+                        name: 'project 2'
+                    }
+                ]
+            },
+
+            purchasedItems: {
+                523: [
+                    {
+                        name: 'adidas sneakers #24124'
+                    },
+                    {
+                        name: 'Jack Daniel\'s 18 years'
+                    }
+                ]
+            }
+
+        };
+
+        this.getData = function (resourceName) { return _data[resourceName]; };
+    }
+
+    function PageController($scope, api) {}
+
+
+    angular.module('app', ['ng', 'ngSanitize'])
+        .service('api', [api])
+        .controller('PageController', ["$scope","api", PageController]);
+
+
+
+
+}).call(window && typeof window.document !== 'undefined' ? window : global);
+
